39d320117fb15d9d4111490ed4233e858c7c613a
robertpearcy <robertpearcy@moscowmail.com>
2018-05-30 21:51:23 -0700

    This is my Day 1 Repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,12 @@
 //day 1
 var myFirst = "Robert";
-
 var myLast = "Pearcy-German"
 
 console.log(myFirst);
-
 console.log(myLast);
 
 var myHope = "Is that one day I can take all of the knowledge i've learned and use it to make a difference.";
-
 var myHobby = "I like to learn about knew tech that is coming out lately I've be studying about augmented reality and Automated Machine Learning ask me for more information. I also like to fish and go backpacking deep into yosemite.";
 
 console.log(myHope);
-
 console.log(myHobby);
