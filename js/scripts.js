2a4cd71bdd5259a59f658e790d53d7e771d298d1
robertpearcy <robertpearcy@moscowmail.com>
2018-06-04 19:42:39 -0700

    d5

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,18 @@ var myHobby = "I like to learn about knew tech that is coming out lately I've be
 
 console.log(myHope);
 console.log(myHobby);
+
+var firstName = prompt("What is your first name?", "Robert");
+var lastName = prompt("What is your last name?", "Pearcy-German");
+var birthday = new Date(prompt("What is your birthday?", "12/30/1987"))
+
+console.log("Your first name is " + firstName);
+alert("Your last name is " + lastName);
+
+var userAnswer = confirm("Is " + birthday + " your birthday?");
+
+if (userAnswer == true) {
+    alert("Happy Birthday to you when " + birthday + " comes around!!");
+} else {
+    alert("Sorry wrong birthday.");
+}
